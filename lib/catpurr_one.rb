16a1f508c82ce3438aa59e27e4c636f1877f42a3
Ben Ratelade <ratelade.benjamin@gmail.com>
2023-02-09 21:11:39 +1100

    On the raspberry, we need to require all the things

--- lib/catpurr_one.rb
+++ lib/catpurr_one.rb
@@ -2,8 +2,7 @@
 
 require "#{__dir__}/../config/config.rb"
 Dir["#{__dir__}/*.rb"].each do |file|
-  # need to do exclude whatever uses pi_piper on Mac. Macs don't have GPIO drivers.
-  require file unless file.match("motion_sensor_state_watcher.rb")
+  require file
 end
 
 module CatpurrOne
