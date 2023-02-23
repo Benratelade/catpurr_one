899ebfdbe7ef8be31c4617785bae758ccf933269
Ben Ratelade <ratelade.benjamin@gmail.com>
2023-02-23 22:10:43 +1100

    Ah. Underscores.

--- config/config.rb
+++ config/config.rb
@@ -2,8 +2,8 @@ Dir["#{__dir__}/initializers/*.rb"].each { |file| require file }
 
 module CatpurrOne
   module Config
-    AWS_REGION = ENV.fetch("CATPURRONE_AWS_REGION", "us-east-1")
-    AWS_S3_BUCKET_NAME = ENV.fetch("CATPURRONE_AWS_S3_BUCKET_NAME", "howdooai.catpurrone-test")
+    AWS_REGION = ENV.fetch("CATPURR_ONE_AWS_REGION", "us-east-1")
+    AWS_S3_BUCKET_NAME = ENV.fetch("CATPURR_ONE_AWS_S3_BUCKET_NAME", "howdooai.catpurrone-test")
     AWS_S3_UNPROCESSED_IMAGES_DIRECTORY = "unprocessed/"
   end
 end
